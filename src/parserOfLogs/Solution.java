6576b7adb7daf46f5766672f24108a347288d2bc
GrNata <gr.natalij@gmail.com>
2019-12-13 13:50:37 +0300

    git pull

--- src/parserOfLogs/Solution.java
+++ src/parserOfLogs/Solution.java
@@ -42,6 +42,50 @@ public class Solution {
             System.out.println("getIPsForStatus__________________________________________________________________");
             System.out.println(logParser.getIPsForStatus(Status.ERROR, null, null));
             System.out.println(logParser.getIPsForStatus(Status.OK, date2, date3));
+
+            System.out.println("\ngetAllUsers__________________________________________________________________");
+            System.out.println(logParser.getAllUsers());
+            System.out.println("\ngetNumberOfUsers__________________________________________________________________");
+            System.out.println(logParser.getNumberOfUsers(null, null));
+            System.out.println("c "+ date1+" по "+date3);
+            System.out.println(logParser.getNumberOfUsers(date1, date3));
+            System.out.println("\ngetNumberOfUserEvents__________________________________________________________________");
+            System.out.println(logParser.getNumberOfUserEvents("Eduard Petrovich Morozko", null, null));
+            System.out.println("c "+ date1+" по "+date2);
+            System.out.println(logParser.getNumberOfUserEvents("Eduard Petrovich Morozko", date1, date2));
+            System.out.println("\ngetUsersForIP__________________________________________________________________");
+            System.out.println(logParser.getUsersForIP("127.0.0.1", null, null));
+            System.out.println("c "+ date1+" по "+date3);
+            System.out.println(logParser.getUsersForIP("127.0.0.1", date1, date3));
+            System.out.println("\ngetLoggedUsers__________________________________________________________________");
+            System.out.println(logParser.getLoggedUsers(null, null));
+            System.out.println("c "+ date1+" по "+date3);
+            System.out.println(logParser.getLoggedUsers(date1, date3));
+            System.out.println("\ngetDownloadedPluginUsers__________________________________________________________________");
+            System.out.println(logParser.getDownloadedPluginUsers(null, null));
+            System.out.println("c "+ date1+" по "+date2);
+            System.out.println(logParser.getDownloadedPluginUsers(date1, date2));
+            System.out.println("\ngetWroteMessageUsers__________________________________________________________________");
+            System.out.println(logParser.getWroteMessageUsers(null, null));
+            System.out.println("c "+ date1+" по "+date2);
+            System.out.println(logParser.getWroteMessageUsers(date1, date2));
+            System.out.println("\ngetSolvedTaskUsers__________________________________________________________________");
+            System.out.println(logParser.getSolvedTaskUsers(null, null));
+            System.out.println("c "+ date3+" по "+null);
+            System.out.println(logParser.getSolvedTaskUsers(date3, null));
+            System.out.println("\ngetSolvedTaskUsers - NUMBER__________________________________________________________________");
+            System.out.println(logParser.getSolvedTaskUsers(null, null, 15));
+            System.out.println("c "+ date3+" по "+null);
+            System.out.println(logParser.getSolvedTaskUsers(date3, null, 18));
+            System.out.println("\ngetDoneTaskUsers__________________________________________________________________");
+            System.out.println(logParser.getDoneTaskUsers(null, null));
+            System.out.println("c "+ date1+" по "+null);
+            System.out.println(logParser.getDoneTaskUsers(date1, null));
+            System.out.println("\ngetDoneTaskUsers - NUMBER__________________________________________________________________");
+            System.out.println(logParser.getDoneTaskUsers(null, null, 15));
+            System.out.println("c "+ date1+" по "+null);
+            System.out.println(logParser.getDoneTaskUsers(date1, null, 48));
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
